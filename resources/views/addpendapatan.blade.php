a3b6cba2b109a56d0b3a207e8a785bc97b483ed8
DidikH <com.didikhariyanto@gmail.com>
2021-03-16 04:08:37 +0700

    #4 - Perbaikan comboBox Selected

--- resources/views/addpendapatan.blade.php
+++ resources/views/addpendapatan.blade.php
@@ -17,17 +17,22 @@
                             <div class="row">
                                 <div class="col-sm-6">
 
-
                                     <div class="form-group">
                                         <label>Ladang</label>
                                         <select name="id_ladang" class="form-control">
-                                            <option>Please Select ...</option>
+                                            <option value="">Please Select ...</option>
                                             @foreach ($ladang as $item)
-                                            <option value="{{ $item->id_ladang }}">{{ $item->nama }}</option>
+                                            <option value="{{ $item->id_ladang }}"{{ old('id_ladang') == $item->id_ladang ? 'selected' : null }}>{{ $item->nama }}</option>
                                             @endforeach
                                         </select>
-
+                                        <div class="text-danger">
+                                            @error('id_ladang')
+                                                {{ $message }}
+                                            @enderror
+                                        </div>
                                     </div>
+
+
                                     <div class="form-group">
                                         <label>Panen Ke</label>
                                         <input name="nama" class="form-control" value="{{ old('nama') }}">
@@ -37,6 +42,8 @@
                                             @enderror
                                         </div>
                                     </div>
+
+
                                     <div class="form-group">
                                         <label>Jumlah Pendapatan</label>
                                         <input type="number" name="amount" class="form-control" value="{{ old('amount') }}">
@@ -46,6 +53,8 @@
                                             @enderror
                                         </div>
                                     </div>
+
+
                                     <div class="form-group">
                                         <label>Tanggal Dibuat</label>
                                         <input type = "date" name="create_date" class="form-control" value="{{ old('create_date') }}">
@@ -55,6 +64,8 @@
                                             @enderror
                                         </div>
                                     </div>
+
+
                                     <div class="form-group">
                                         <label>Tanggal Ubah</label>
                                         <input type = "date" name="update_date" class="form-control" value="{{ old('update_date') }}">
@@ -64,6 +75,8 @@
                                             @enderror
                                         </div>
                                     </div>
+
+
                                     <div class="form-group">
                                         <button class="btn btn-primary btn-sm">Simpan</button>
                                         <a href="/pendapatan" class="btn -list-alt btn-success ">Kembali</a>
