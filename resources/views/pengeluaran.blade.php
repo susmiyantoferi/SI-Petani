dd9f9162d150a22e5c8c76d35ab765f950b7a779
DESKTOP-TV681AN\Feri Susmiyanto <irefmcr@gmail.com>
2021-03-17 12:37:59 +0700

    #11 - Penambahan Fitur Pencarian Di Pengeluaran

--- resources/views/pengeluaran.blade.php
+++ resources/views/pengeluaran.blade.php
@@ -16,6 +16,24 @@
                             {{ session('pesan') }}.
                           </div>
                         @endif
+
+                                               {{-- Box pencarian --}}
+
+                                                <div class="box-tools">
+                                                  <form th:action="@{/karyawan/list}" method="get">
+                                                      <div class="input-group input-group-sm" style="width: 250px;">
+                                                          <input type="text" name="value" class="form-control pull-right"
+                                                                 placeholder="Pencarian Berdasarkan Nama" th:if="${key} == null"/>
+                                                          <div class="input-group-btn">
+                                                              <button type="submit" class="btn btn-default"><i class="fa fa-search"></i></button>
+                                                          </div>
+                                                      </div>
+                                                  </form>
+                                              </div>
+                                          </div>
+                      
+                                          {{-- Box pencarian --}}
+
                         <div class="box-body">
                             <table id="example2" class="table table-bordered table-hover">
                                 <thead>
