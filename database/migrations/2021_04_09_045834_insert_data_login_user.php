cd16583dd053bb2e2769978cef11f8cbafdbe8d8
DidikH <com.didikhariyanto@gmail.com>
2021-04-09 12:17:38 +0700

    #35 - Penambahan database migrasi user - username : admin@admin.com - pw : 12345678

---
+++ database/migrations/2021_04_09_045834_insert_data_login_user.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+class InsertDataLoginUser extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::statement("
+
+insert into users (id, name, email, email_verified_at, password, remember_token, created_at, updated_at)
+values
+('1', 'Feri Susmiyanto', 'admin@admin.com', null, '$2y$10$7esGDBjvTRVW6OgsbuAH1.j1OUHRKkKCjt8P6vb4wG9OgqHjyUAT.',
+null, '2021-04-09 05:00:42', '2021-04-09 05:00:42' );
+        ");
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
